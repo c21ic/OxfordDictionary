8fc544e7e3c43498f143ef861243a6405597db27
ineschomnalez <c21ic@dalton.org>
2018-02-05 11:28:23 -0500

    started game

--- src/test/DictionaryTester.java
+++ src/test/DictionaryTester.java
@@ -12,17 +12,20 @@ public class DictionaryTester {
 		String randWordDef = Dictionary.getDefinition(randWord);
 
 		// Show random word and definition
-        System.out.println("The word is " + randWord);
+        //System.out.println("The word is " + randWord);
 		System.out.println("The definition is " + randWordDef);
 
 		// Other information
-        System.out.print("Here is the word without vowels: ");
-        String noVowels = randWord.replaceAll("[aeiou]", "-");
-        System.out.println(noVowels);
+       // System.out.print("Here is the word without vowels: ");
+       // String noVowels = randWord.replaceAll("[aeiou]", "-");
+        //System.out.println(noVowels);
 
         System.out.println("The word has " + randWord.length() + " letters.");
 
         System.out.println("The first letter is " + randWord.charAt(0) + ".");
+        System.out.println("Can you guess what the word is?");
+
+
 
 		scan.close();
 	}
--- src/test/GuessingGame.java
+++ src/test/GuessingGame.java
@@ -1,11 +1,40 @@
 package test;
 
+import app.Dictionary;
+
+import java.util.Scanner;
+
 public class GuessingGame {
 
     public static void main(String[] args) {
-        // Make a guessing game using the Dictionary Library. See Dictionary Tester for inspiration.
+        Scanner scan = new Scanner(System.in);
+        String randWord = Dictionary.getRandomWord();
+        String randWordDef = Dictionary.getDefinition(randWord);
 
-        // You can be inventive!
+        // Show random word and definition
+        //System.out.println("The word is " + randWord);
+        System.out.println("The definition is " + randWordDef);
+
+        // Other information
+        // System.out.print("Here is the word without vowels: ");
+        // String noVowels = randWord.replaceAll("[aeiou]", "-");
+        //System.out.println(noVowels);
+
+        System.out.println("The word has " + randWord.length() + " letters.");
+
+        System.out.println("The first letter is " + randWord.charAt(0) + ".");
+        System.out.println("Can you guess what the word is?");
+        String guess = scan.nextLine();
+        if (guess.equals(randWord)) {
+            System.out.println("Great job! You got it right.");
+        }
+        else {
+            System.out.println("You got it wrong, the word is " +randWord + " game over.");
+        }
+
+
+        scan.close();
     }
+
 
 }
