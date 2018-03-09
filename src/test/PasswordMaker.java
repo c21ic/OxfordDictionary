b1061f8765145df4421133e8eb21d931cd491d57
ineschomnalez <c21ic@dalton.org>
2018-03-09 09:40:18 -0500

    started game

--- src/test/PasswordMaker.java
+++ src/test/PasswordMaker.java
@@ -1,24 +1,82 @@
 package test;
 
+import app.Dictionary;
+
+import java.util.Scanner;
+
 public class PasswordMaker {
 
+
     public static void main(String[] args) {
-        // Use the Dictionary library to create a secure password.
+        Scanner scan = new Scanner(System.in);
+        String randWord = Dictionary.getRandomWord();
+        String randWord2 = Dictionary.getRandomWord();
+        String password = randWord + randWord2;
+        int passwordlength = password.length();
 
-        // 1. Get 2 random words and put them together.
-        // 1a. Make sure it is at least 8 characters long. If not, do it again.
+        password = (randWord + randWord2);
+        while (passwordlength <= 8) {
+            randWord = Dictionary.getRandomWord();
+            randWord2 = Dictionary.getRandomWord();
+            password = randWord + randWord2;
+            passwordlength = password.length();
+        }
+        String novowels = randWord;
+        String novowels1 = randWord2;
 
-        // 2. Replace the vowels with numbers following these rules:
-        //    a becomes 4
-        //    e becomes 3
-        //    i becomes 1
-        //    0 becomes 0
-        //    u becomes 6
+        novowels = novowels.replaceAll("a", "4");
+        novowels1 = novowels1.replaceAll("a", "4");
+        novowels = novowels.replaceAll("e", "3");
+        novowels1 = novowels1.replaceAll("e", "3");
+        novowels = novowels.replaceAll("i", "1");
+        novowels1 = novowels1.replaceAll("i", "1");
+        novowels = novowels.replaceAll("o", "0");
+        novowels1 = novowels1.replaceAll("o", "0");
+        novowels = novowels.replaceAll("u", "6");
+        novowels1 = novowels1.replaceAll("u", "6");
+        password = novowels + novowels1;
 
-        // 3. Capitalize the first letter
+        String upper = password.toUpperCase();
+        System.out.println("Your password is " + password);
+        System.out.println("Do you like this password?");
+        String response = scan.nextLine();
 
-        // 4. Tell the person their password.
+        if (response.equals("yes")) {
+            System.out.println("Great");
+        } else {
+            System.out.println("Ok, you will be given a new password.");
+            String randWord3 = Dictionary.getRandomWord();
+            String randWord4 = Dictionary.getRandomWord();
 
-        // BONUS 5. Ask the person if they want another one and make another one.
+            String password1 = (randWord3 + randWord4);
+            int password1length = password1.length();
+            while (password1length <= 8) {
+                randWord3 = Dictionary.getRandomWord();
+                randWord4 = Dictionary.getRandomWord();
+                password1 = randWord3 + randWord4;
+                password1length = password1.length();
+            }
+
+            String novowels2 = randWord3;
+            String novowels3 = randWord4;
+
+            novowels2 = novowels2.replaceAll("a", "4");
+            novowels3 = novowels3.replaceAll("a", "4");
+            novowels2 = novowels2.replaceAll("e", "3");
+            novowels3 = novowels3.replaceAll("e", "3");
+            novowels2 = novowels2.replaceAll("i", "1");
+            novowels3 = novowels3.replaceAll("i", "1");
+            novowels2 = novowels2.replaceAll("o", "0");
+            novowels3 = novowels3.replaceAll("o", "0");
+            novowels2 = novowels2.replaceAll("u", "6");
+            novowels3 = novowels3.replaceAll("u", "6");
+            password1 = novowels2 + novowels3;
+
+            String upper1 = password1.toUpperCase();
+            System.out.println("Your password is " + password1);
+
+        }
     }
+
 }
+
--- src/test/guessingGameProject.java
+++ src/test/guessingGameProject.java
@@ -7,12 +7,83 @@ import java.util.Scanner;
 public class guessingGameProject {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String randWord = Dictionary.getRandomWord();
-        String randWordDef = Dictionary.getDefinition(randWord);
+        int score = 0;
 
         // Show random word and definition
         //System.out.println("The word is " + randWord);
-        System.out.println("The definition is " + randWordDef);
+        while (score <5) {
+            String randWord = Dictionary.getRandomWord();
+            String randWordDef = Dictionary.getDefinition(randWord);
+            System.out.println("Do you want to play the easy, medium or hard level?");
+            String answer = scan.nextLine();
+            if (answer.equals("easy")) {
+                System.out.println("The definition is " + randWordDef);
+                System.out.println("The word has " + randWord.length() + " letters.");
+
+                System.out.println("The first letter is " + randWord.charAt(0) + ".");
+                System.out.println("Can you guess what the word is?");
+                String guess = scan.nextLine();
+                if (guess.equals(randWord)) {
+                    System.out.println("Great job! You got it right.");
+                    score = score + 1;
+                    System.out.println("Your score now equals " + score + "!");
+                } else {
+                    System.out.println("You got it wrong, the word was actually " + randWord + " better luck next time.");
+                    score = score - 1;
+                    System.out.println("Your score now equals " + score + "!");
+                }
+            } else if (answer.equals("medium")) {
+                System.out.println("Would you rather get the word length or the first letter?");
+                String answertoquestion = scan.nextLine();
+                if(answertoquestion.equals("first letter")){
+                    System.out.println("The definition is " + randWordDef);
+                    System.out.println("The first letter of the word is " + randWord.charAt(0));
+                }
+                else if(answertoquestion.equals("word length")){
+                    System.out.println("The definition is " + randWordDef);
+                    System.out.println("The length of the word is " + randWord.length());
+                }
+               System.out.println("Can you guess what the word is?");
+                String response = scan.nextLine();
+                if(response.equals(randWord)){
+                    System.out.println("Great job! You got it right.");
+                    score = score + 2;
+                    System.out.println("Your score now equals " + score);
+                }
+                else{
+                    System.out.println("You got it wrong, the word was actually " + randWord + " better luck next time.");
+                    score = score - 2;
+                    System.out.println("Your score now equals " + score);
+                }
+
+            } else if (answer.equals("hard")) {
+                String randWord1 = Dictionary.getRandomWord();
+                String randWordDef1 = Dictionary.getDefinition(randWord1);
+                System.out.println("The definition is " + randWordDef1);
+                System.out.println("Can you guess what the word is?");
+                String guess1 = scan.nextLine();
+                if (guess1.equals(randWord1)) {
+                    System.out.println("Great job! You got it right.");
+                    score = score + 3;
+                    System.out.println("Your score now equals " + score + " !");
+                } else {
+                    System.out.println("You got it wrong, the word was actually " + randWord1 + " better luck next time.");
+                    score = score - 3;
+                    System.out.println("Your score now equals " + score + " !");
+                }
+
+            } else {
+                System.out.println("sorry that's not an option.");
+            }
+        }
+        if (score >= 5) {
+            System.out.println("Great job! You got a score of " + score + "! You won the game :)");
+        }
+    }
+}
+
+
+//System.out.println("The definition is " + randWordDef);
 
         /*
         Other information
@@ -21,19 +92,4 @@ public class guessingGameProject {
         System.out.println(noVowels);
         */
 
-        System.out.println("The word has " + randWord.length() + " letters.");
 
-        System.out.println("The first letter is " + randWord.charAt(0) + ".");
-        System.out.println("Can you guess what the word is?");
-        String guess = scan.nextLine();
-        if (guess.equals(randWord)) {
-            System.out.println("Great job! You got it right.");
-        }
-        else {
-            System.out.println("You got it wrong, the word was actually " +randWord + " better luck next time.");
-        }
-
-
-        scan.close();
-    }
-}
